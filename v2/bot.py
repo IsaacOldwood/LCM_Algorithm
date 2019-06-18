3a2b9e7401d51c7343a02a454748cc76bebbc077
Isaac Oldwood <isaac.oldwood@gmail.com>
2019-06-18 21:30:29 +0100

    Reduced Factor Checking by 2 cycles since current number and 1 are always factors

--- v2/bot.py
+++ v2/bot.py
@@ -21,11 +21,13 @@ current_number=int(current_number)
 consecutive_factors=False
 while consecutive_factors==False:
     print(f'Current Number; {current_number}')
-    factors=[]
+    factors=[1] #1 is always a factor
     
-    for i in range(1,(current_number+1)): #Test All Possible Numbers
+    for i in range(2,(current_number)): #Test All Possible Numbers
         if current_number%i == 0: #Test if i is a factor of the current number
             factors.append(i) #If a factor then apppend to list of factors
+
+    factors.append(current_number)
     
     
     #Check If Factors are consecutive
@@ -40,9 +42,6 @@ while consecutive_factors==False:
     if consecutive_factors==False:
         current_number+=1
 
-
-
-    
 
 if consecutive_factors==True:
     factor_string=','.join(str(factor) for factor in factors)
